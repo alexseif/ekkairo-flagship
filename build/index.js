8021077b2582d7c85aac60b717d0f0a0d00eded5
Alex Seif <alex.seif@gmail.com>
2026-08-18 17:08:46 +0300

    feat(build): setup @wordpress/scripts build pipeline and compiled assets

---
+++ build/index.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("EKK Flagship Theme initialized.")})();
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Main Theme Asset Entrypoint (Source)
+ * Compiled via @wordpress/scripts into build/index.js
+ */
+
+import './index.scss';
+
+// Theme JS initialization
+console.log('EKK Flagship Theme initialized.');
